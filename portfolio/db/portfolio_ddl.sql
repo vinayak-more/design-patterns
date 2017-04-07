ecad30593025bb37af439724960f1eaabbbdd2b5
Vinayak More <vinayak.more@infibeam.net>
2017-04-08 00:10:35 +0530

    porfolio db

--- portfolio/db/portfolio_ddl.sql
+++ portfolio/db/portfolio_ddl.sql
@@ -2,8 +2,53 @@ CREATE USER 'portfolio'@'localhost' IDENTIFIED BY 'portfolio';
 
 GRANT CREATE,DELETE,INSERT,SELECT,UPDATE ON portfolio.* TO 'portfolio'@'localhost';
 
+CREATE TABLE user (
+	user_id bigint(20) PRIMARY KEY,
+	username varchar(255) NOT NULL,
+	password varchar(50) NOT NULL,
+	INDEX username (`username`)
+);
 
-REVOKE CREATE,DELETE,INSERT ON *.* FROM 'portfolio'@'localhost';
+CREATE TABLE user_basic_details(
+	rid bigint(20) PRIMARY KEY AUTO_INCREMENT,
+	user_id bigint(20),
+	name varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	CONSTRAINT fk_user FOREIGN KEY (user_id)
+  	REFERENCES user(user_id)
+  	ON DELETE CASCADE
+  	ON UPDATE CASCADE 
+);
 
+CREATE TABLE user_transaction(
+	rid bigint(20) PRIMARY KEY AUTO_INCREMENT,
+	user_id bigint(20),
+	trasaction_datetime datetime,
+	type varchar(10) NOT NULL,
+	market varchar(10) NOT NULL,
+	symbol varchar(20) NOT NULL,
+	quantity int(10) NOT NULL,
+	price_per_stock double(10,5),
+	price_in_total double(15,5),
+	CONSTRAINT fk_user_transc FOREIGN KEY (user_id)
+  	REFERENCES user(user_id)
+  	ON DELETE CASCADE
+  	ON UPDATE CASCADE 
+);
 
-DROP USER 'porfolio'@'localhost';
+CREATE TABLE user_folio(
+	rid bigint(20) PRIMARY KEY AUTO_INCREMENT,
+	user_id bigint(20),
+	symbol varchar(20) NOT NULL,
+	quantity int(10) NOT NULL,
+	investment double(20,5),
+	current_value double(20,5),
+	change_value double(20,5),
+	change_percent double(5,5),
+	todays_gain double(5,5),
+	last_updated datetime,
+	CONSTRAINT fk_user_folio FOREIGN KEY (user_id)
+  	REFERENCES user(user_id)
+  	ON DELETE CASCADE
+  	ON UPDATE CASCADE 
+);
